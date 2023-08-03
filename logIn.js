81143054f3d92b77f3abf436e0fdbc944df7eb1c
HARISHKUMAR S <harishkumar.ct20@bitsathy.ac.in>
2023-08-03 11:13:38 +0530

    first commit

---
+++ logIn.js
@@ -0,0 +1,11 @@
+// const check = document.getElementById('check');
+
+function showPassword()
+{
+    var pwd = document.getElementById('password');
+    if(pwd.type ==='password')
+        pwd.type  = 'text';
+    else
+        pwd.type = "password"; 
+
+}
